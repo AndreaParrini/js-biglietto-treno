d12f53a8bce9ed99ddb5e2bf55d1e1c37dee9700
AndreaParrini <andrea.parrini26@gmail.com>
2023-11-18 14:23:31 +0100

    add function
    add prompt and if in script to determinate price of ticket

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
-console.log('Hello Word');
+const userKM = parseInt(prompt('Quanti chilometri vuoi percorrere?'));
+const userAge = parseInt(prompt('Inersisce la tua età'));
+
+console.log(userKM, userAge);
+
+let ticketPrice = userKM * 0.21;
+
+console.log(ticketPrice);
+
+let ticketDiscount = 0;
+
+if (userAge < 18 ){
+
+    ticketDiscount = ticketPrice * 0.2;
+
+} else if (userAge >= 65) {
+
+    ticketDiscount = ticketPrice * 0.4;
+
+}
+
+let ticketFullPrice = ticketPrice - ticketDiscount;
+
+alert(ticketFullPrice.toFixed(2));
